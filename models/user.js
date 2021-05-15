95c0f36b115d67226939a8c47742a54bfddbc73d
HaythemRomdhani <romdhanihaythem1991@gmail.com>
2021-05-15 23:23:07 +0100

    restapi

---
+++ models/user.js
@@ -0,0 +1,11 @@
+let mongoose=require('mongoose')
+
+let person = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    }
+    });
+
+  
+  module.exports= mongoose.model('person',person)
